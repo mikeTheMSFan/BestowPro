5ccd6d990e0f334681815d1f860b39f06b714a07
mikeTheMSFan <64504996+mikeTheMSFan@users.noreply.github.com>
2021-08-27 20:08:11 -0400

    main functionality completed

--- js/site.js
+++ js/site.js
@@ -5,48 +5,149 @@ function getValues() {
   let interestRate = document.getElementById('interestRate').value;
 
   //Validate that the input is a number
-  if (parseInt(loanAmount) && parseInt(monTerm) && parseInt(interestRate)) {
+  if (parseInt(loanAmount) && parseInt(monTerm) && parseFloat(interestRate)) {
+    //reassign strings as numbers
     loanAmount = parseInt(loanAmount);
     monTerm = parseInt(monTerm);
-    interestRate = parseInt(interestRate);
-    calculatePayments(loanAmount, monTerm, interestRate);
+    interestRate = parseFloat(interestRate);
+
+    //Construct an array of objects with payment info
+    const paymentArr = calculatePayments(loanAmount, monTerm, interestRate);
+
+    //Send array to display function
+    displayPaymentInformation(paymentArr, loanAmount);
   } else {
     alert('Please check your input and make sure you have only used numbers.');
   }
 }
 
 //Calculate Monthly Payment, Principal, Interest, and cost
-function calculatePayments(loan, monTerm, interest) {
-  //Intrest converted to percentage
-  interest = interest / 100;
+function calculatePayments(loan, months, rate) {
+  //Interest Payment
+  const c = rate / 1200;
 
-  //Rate computed from interest
-  rate = interest / 12;
+  //Power of morgage payment formula
+  const temp = Math.pow(1 + c, months);
 
-  //Add one to rate
-  let rateCoEfficient = rate + 1;
-  let rateProduct = rate + 1;
+  //Monthly Payments is : loan * c * (1 + c) ^ months / (1 + c) ^ months -1
+  //Represented in JS Below.
+  let monthlyPayment = (loan * c * temp) / (temp - 1);
 
-  //Raise to the power of the monthly Payments
-  rateCoEfficient = Math.pow(rateCoEfficient, monTerm);
-  rateProduct = Math.pow(rateProduct, monTerm);
+  //Sets initial balance for calculations
+  let remainingBalance = loan;
 
-  //Simplify...
-  const smallCalc =
-    Math.round(((rate * rateCoEfficient) / (rateProduct - 1)) * 1000000) /
-    1000000;
+  //Tallys total  for life of loan
+  let totalInterestPaid = 0;
 
-  //Multiply by the loan
-  monthly = loan * smallCalc;
+  //Loan information for reach month is set here
+  const loanInfo = [];
 
-  //Fill object with calculated values for display
-  const loanInfo = {};
-  for (let i = 0; i <= monTerm; i++) {
-    loanInfo.month = i;
-    loanInfo.balance = loan - loanInfo.principal;
-    loanInfo.interest = interest / 1200;
-    loanInfo.principal = monthly - loanInfo.interest;
+  //Loops over months until 0
+  //Calculates and stores information for display in array
+  for (let i = 1; i <= months; i++) {
+    let interestPaid = 0;
+    let principal = 0;
+
+    interestPaid = remainingBalance * c;
+    principal = monthlyPayment - interestPaid;
+    remainingBalance -= principal;
+    totalInterestPaid += interestPaid;
+
+    // .toLocaleString('us-US', {
+    //   style: 'currency',
+    //   currency: 'USD',
+    // })
+
+    loanInfo.push({
+      month: i,
+      payments: monthlyPayment,
+      principal: principal,
+      interest: interestPaid,
+      totalInterest: totalInterestPaid,
+      balance: remainingBalance,
+    });
   }
+  return loanInfo;
 }
 
 //Display Result
+
+function displayPaymentInformation(paymentArr, principal) {
+  let template = '';
+
+  //get table body element
+  const tableBody = document.getElementById('results');
+  //get templates
+  const templateRow = document.getElementById('bpTemplate');
+
+  //last index to get final values
+  const lastIndex = paymentArr.length - 1;
+
+  //reset table
+  tableBody.innerHTML = '';
+
+  //Sets monthly Payment in Total
+  document.getElementById('monthlyTotal').innerHTML = paymentArr[
+    lastIndex
+  ].payments.toLocaleString('us-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  //Sets Principal
+  document.getElementById('totalPrincipal').innerHTML =
+    principal.toLocaleString('us-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+  //Sets total Interest
+  document.getElementById('totalInterest').innerHTML = paymentArr[
+    lastIndex
+  ].totalInterest.toLocaleString('us-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  //Sets total Cost
+  document.getElementById('totalCost').innerHTML = (
+    paymentArr[lastIndex].totalInterest + principal
+  ).toLocaleString('us-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  for (let i = 0; i < paymentArr.length; i++) {
+    //create a fragment or copy of the template made in HTML
+    let tableRow = document.importNode(templateRow.content, true);
+
+    //Gets just the tb elements and puts them into an array
+    let rowCols = tableRow.querySelectorAll('td');
+
+    //Fills 'td' array with data from the passed in array.
+    rowCols[0].textContent = paymentArr[i].month;
+    rowCols[1].textContent = paymentArr[i].payments.toLocaleString('us-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    rowCols[2].textContent = paymentArr[i].principal.toLocaleString('us-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    rowCols[3].textContent = paymentArr[i].interest.toLocaleString('us-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    rowCols[4].textContent = paymentArr[i].totalInterest.toLocaleString(
+      'us-US',
+      { style: 'currency', currency: 'USD' }
+    );
+    rowCols[5].textContent = paymentArr[i].balance.toLocaleString('us-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    //Appends filled 'td' rows to the HTML Table
+    tableBody.appendChild(tableRow);
+  }
+}
